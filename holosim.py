23718a26a7ccd83a67273a8e46b53b61a8097111
Yuri Volkov <volkov.yuri.alex@gmail.com>
2024-07-24 22:23:42 +0200

    Added light intensity function for one pixel

--- holosim.py
+++ holosim.py
@@ -14,6 +14,20 @@ import numpy as np
 from mayavi import mlab
 
 
+
+
+class Matrix:
+
+    values = np.zeros((3, 3), np.uint8)
+    pixel_x_size = 0.5
+    pixel_y_size = 0.5
+
+    def x_pos(self):
+        return 0
+    
+    def y_pos(self):
+        return 0
+
 # def plane(x: np.ndarray):
 #     res = np.zeros(x.shape)
 #     for (val, arg) in zip(res.flat, x.flat):
@@ -30,17 +44,30 @@ def plane(x):
 plane_v = np.vectorize(plane)
 
 
-x, y, z = np.mgrid[-5:5:50j, -5:5:50j, 0:10:50j]
+vox_x_pos, vox_y_pos, vox_z_pos = np.mgrid[-100:100:100j, -100:100:100j, 0:500:100j]
 # x1, y1 = np.mgrid[-5:5:500j, -5:5:500j]
-scalar = np.sin(np.sqrt(x**2 + y**2 + z**2))
+# scalar = np.sin(np.sqrt(x**2 + y**2 + z**2))
 
 # scalar = np.sinc(x)*np.sinc(y) #/(np.abs(z)+1)
 
 # scalar = plane_v(z)
 # print(scalar)
 
+radius = np.sqrt(vox_x_pos**2 + vox_y_pos**2 + vox_z_pos**2)
+# in micrometer
+wave_len = 0.5
+wave_number = 2 * np.pi / wave_len
+pixel_x_size = 10
+pixel_y_size = 10
+
+light_phasor = np.exp(1j * wave_number * radius) / radius           \
+    * np.sinc(wave_number * pixel_y_size * vox_x_pos / 2 / radius)  \
+    * np.sinc(wave_number * pixel_x_size * vox_y_pos / 2 / radius)
+light_intensity = np.abs(light_phasor)**2
+
+
 fig = mlab.figure()
-contour = mlab.contour3d(x, y, z, scalar, colormap='winter')
+contour = mlab.contour3d(vox_x_pos, vox_y_pos, vox_z_pos, light_intensity, colormap='winter')
 
 
 s = np.random.random((20, 20))
